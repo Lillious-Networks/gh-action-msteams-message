b68b6689d3ce72f94802330d57889e42cf1a75ed
Logan Brown <logan.brown@tylertech.com>
2024-06-26 14:34:55 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import fetch from 'node-fetch';
+
+let sendMessage = async () => {
+    let msteams_webhook = core.getInput('webhook');
+    let msteams_title = core.getInput('title');
+    let msteams_subtitle = core.getInput('subtitle');
+    let msteams_color = core.getInput('color');
+    let msteams_message = core.getInput('message');
+
+    let body = {
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": msteams_color,
+        "title": msteams_title,
+        "text": msteams_subtitle,
+        "sections": [
+            {
+                "facts": [
+                    {
+                        "name": "",
+                        "value": msteams_message
+                    }
+                ]
+            }
+        ]
+    };
+
+    let response = await fetch(msteams_webhook, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+}
+
+try {
+    let response = await sendMessage(msteams_webhook, msteams_title, msteams_subtitle, msteams_color, msteams_message);
+
+    if (response) {
+        core.setOutput('response', response);
+    } else {
+        core.setFailed(`Failed to send message: ${response.message}`);
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Microsoft Teams Message Action
+
+This action sends a message to a Microsoft Teams channel
+
+## Inputs
+
+### `title`
+
+**Required** The title of the message.
+
+### `subtitle`
+
+The subtitle of the message. Default `" "`.
+
+### `color`
+
+The color of the message. Default `"#000000"`.
+
+### `message`
+
+**Required** The message to send.
+
+### `webhook`:
+
+**Required** The webhook URL to send the message to.
+
+## Example usage
+
+```yaml
+uses: actions/gh-action-msteams-message@v1.0.0
+with:
+  title: 'Build & Release'
+  subtitle: 'Product Version: v1.0.2'
+  color: '#000000'
+  message: 'Successfully built and released Product using version v.1.0.2'
+  webhook: '${{ secrets.msteams_webhook }}'
+```
+
+
+**Required** The name of the person to greet. Default `"World"`.
